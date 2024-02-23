91f6e55aac478410a1fef38b88197c2d980255fb
GustavoRamosCapistrano <sba23064@student.cct.ie>
2024-02-23 12:52:37 +0000

    Add first part
    Created instanced fields
    contructor with defaut value and also once contructor with input information
    created nextEmpNum to increment every new employee
    created methods and also challenge to check for valid email

--- src/Employee.java
+++ src/Employee.java
@@ -8,5 +8,49 @@
  * @author grc29
  */
 public class Employee {
-    
+
+    private String name;
+    private String email;
+    private int empNum;
+
+    private static int nextEmpNum = 1;
+
+    public Employee() {
+        this.name = "Default";
+        this.email = "default@email.com";
+        this.empNum = nextEmpNum++;
+    }
+
+    public Employee(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.empNum = nextEmpNum++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getEmpNum() {
+        return empNum;
+    }
+
+    // Method to set email with validation
+    public void setEmail(String email) {
+        if (email.length() > 3) {
+            this.email = email;
+        } else {
+            System.out.println("Email must be longer than 3 characters.");
+        }
+    }
+
+    // Method to get nextEmpNum
+    public static int getNextEmpNum() {
+        return nextEmpNum;
+    }
+
 }
--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -8,5 +8,25 @@
  * @author grc29
  */
 public class EmployeeTest {
-    
+    public static void main(String[] args) {
+        // Create 3 Employee objects
+        Employee emp1 = new Employee("Joe Bloggs", "jb@gmail.com");
+        Employee emp2 = new Employee("Ann Banana", "ab@gmail.com");
+        Employee emp3 = new Employee("Tom Thumb", "tt@gmail.com");
+
+        // Declare and load array with Employee objects
+        Employee[] projectGroup = {emp1, emp2, emp3};
+
+        // Print out the value of nextEmpNum
+        System.out.println("Next Employee Number: " + Employee.getNextEmpNum());
+
+        // Search and display the name of employees with empNum above m
+        int m = 2; // Example value for m
+        System.out.println("Employees with empNum above " + m + ":");
+        for (Employee employee : projectGroup) {
+            if (employee.getEmpNum() > m) {
+                System.out.println(employee.getName());
+            }
+        }
+    }
 }
