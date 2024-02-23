e852cdfecc0a550d982fe1b1d85a600830ccbc18
GustavoRamosCapistrano <sba23064@student.cct.ie>
2024-02-23 13:16:48 +0000

    Created Company.java and add requirements and created company manager to add the menu

--- src/Company.java
+++ src/Company.java
@@ -1,12 +1,64 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author grc29
  */
 public class Company {
-    
+
+    private String companyName;
+    private ArrayList<Employee> staff;
+
+    public Company() {
+        this.companyName = "Default Company";
+        this.staff = new ArrayList<>();
+    }
+
+    public Company(String companyName) {
+        this.companyName = companyName;
+        this.staff = new ArrayList<>();
+    }
+
+    public void addNewStaff(Employee employee) {
+        staff.add(employee);
+    }
+
+    public int getStaffNumber() {
+        return staff.size();
+    }
+
+    public void listEmployees(int empNumThreshold) {
+        Iterator<Employee> iterator = staff.iterator();
+        System.out.println("Employees with empNum above " + empNumThreshold + ":");
+        while (iterator.hasNext()) {
+            Employee employee = iterator.next();
+            if (employee.getEmpNum() > empNumThreshold) {
+                System.out.println(employee.getName());
+            }
+        }
+    }
+    private String username;
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
